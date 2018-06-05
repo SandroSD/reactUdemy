7a4e1985c5767b168f87c914af0b40a9e3fe78dd
SandroSD <sdezerio@soulitsolutions.com>
2018-06-05 07:58:27 -0300

    qcyo

---
+++ Unidad 2 - JS_ES6/index.js
@@ -0,0 +1,9 @@
+const printMyName = (name, age) => {
+    console.log(name);
+    console.log(age);
+  }
+  
+  printMyName("SANDRO", "27");
+  
+  const multiply = (number) => number * 2;  //puede ir sin llaves y no escribir return.
+  console.log(multiply(2));
