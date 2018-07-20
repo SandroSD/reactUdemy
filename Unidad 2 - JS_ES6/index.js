9f0f508047271affbc893a2b310d273232a7bcbe
SandroSD <sdezerio@soulitsolutions.com>
2018-07-20 08:39:54 -0300

    mirar dsd video 65

--- Unidad 2 - JS_ES6/index.js
+++ Unidad 2 - JS_ES6/index.js
@@ -7,3 +7,74 @@ const printMyName = (name, age) => {
   
   const multiply = (number) => number * 2;  //puede ir sin llaves y no escribir return.
   console.log(multiply(2));
+
+  class Person{
+    name ="Max" // property
+    call = () => {} // methods
+  }
+
+  const myPerson = new Person()
+  myPerson.call()
+  console.log(myPerson.name);
+
+  //class Person extends Master ===> Inheritance
+
+  class Human{
+  
+    gender = "Male";
+    
+    printGender = () => {
+      console.log(this.gender);
+    }
+  }
+  
+  class Person extends Human{  
+    
+    name="Sandro";
+    gender="Female";
+      
+    printMyName = () => {
+      console.log(this.name);
+    }
+  }
+  
+  const person = new Person();
+  person.printMyName();
+  person.printGender();
+/**Spread Operator */
+const numbers = [1, 2, 3];
+const newNumbers1 = [...numbers, 4];
+const newNumbers2 = [numbers, 4];
+
+console.log(newNumbers1);
+console.log(newNumbers2);
+
+const person = {
+  name: 'Max';
+};
+
+const newPerson = {
+  ...person,
+  age: 28
+}
+
+console.log(newPerson);
+/**Rest Operator */
+const filter = (...args) => {
+  return args.filter(el => el === 1);
+}
+
+console.log(filter(1, 2, 3));
+
+/**Destructuring */
+const numbers = [1,2,3];
+[num1, num2] = numbers;
+console.log(num1, num2);
+
+const numbers = [1,2,3];
+const doubleNumArray = numbers.map((num) => {
+  return num * 2;
+});
+
+console.log(numbers);
+console.log(doubleNumArray);
