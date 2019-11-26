fd31c3a29ce179ea019660add770222f46a2a4bc
SandroSD <sdezerio@soulitsolutions.com>
2019-11-26 08:22:43 -0300

    Continuar desde el video 471 - Deleting Ingredients

--- React Hooks/hooks-01-starting-project/src/components/Ingredients/Ingredients.js
+++ React Hooks/hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -6,14 +6,51 @@ import Search from './Search';
 
 const Ingredients = () => {
 	const [ userIngredients, setUserIngredients ] = useState([]);
+
+	const filteredIngredientesHandler = useCallback(filteredIngredients => {
+		setUserIngredients(filteredIngredients);
+	}, []);
+
+	//after every render component cycle.
+	/*useEffect( () => {
+		fetch('https://react-hooks-update-9d2bc.firebaseio.com/ingredients.json')
+		.then(response => response.json())
+		.then(responseData => {
+			const loadedIngredients = [];
+			for(const key in responseData) {
+				loadedIngredients.push({
+					id: key,
+					title: responseData[key].title,
+					amount: responseData[key].amount
+				})
+			}
+			setUserIngredients(loadedIngredients);
+		});
+	}, []);*/ //Cuando pasas como segundo parámetro del useEffect [] funciona como componentDidMount y se activa solo una vez luego del primer render.
+
+	useEffect(()=>{
+		console.log('Rendering Ingredients', userIngredients);
+	}, [userIngredients]);
+
 	const addIngredientHandler = ingredient => {
-		setUserIngredients(prevIngredients => [...prevIngredients, {id: Math.random().toString(), ...ingredient}]);
+		fetch('https://react-hooks-update-9d2bc.firebaseio.com/ingredients.json', {
+			method: 'POST',
+			body: JSON.stringify(ingredient),
+			headers: { 'Content-Type': 'application/json' }
+		}).then(response => {
+			return response.json();
+		}).then(responseData => {
+			setUserIngredients(prevIngredients => [
+				...prevIngredients,
+				{ id: responseData.name, ...ingredient }
+			]);
+		})
 	}
 	return (
 		<div className="App">
 		<IngredientForm onAddIngredient={addIngredientHandler} />
 		<section>
-			<Search />
+			<Search	onLoadIngredients={filteredIngredientesHandler} />
 			<IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
 		</section>
 		</div>
--- React Hooks/hooks-01-starting-project/src/components/Ingredients/Search.js
+++ React Hooks/hooks-01-starting-project/src/components/Ingredients/Search.js
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
-  return (
-    <section className="search">
-      <Card>
-        <div className="search-input">
-          <label>Filter by Title</label>
-          <input type="text" />
-        </div>
-      </Card>
-    </section>
-  );
+	const { onLoadIngredients } = props;
+	const [enteredFilter, setEnteredFilter] = useState('');
+	const inputRef = useRef();
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			if(enteredFilter === inputRef.current.value) {
+				const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
+				fetch('https://react-hooks-update-9d2bc.firebaseio.com/ingredients.json' + query)
+				.then(response => response.json())
+				.then(responseData => {
+					const loadedIngredients = [];
+					for(const key in responseData) {
+						loadedIngredients.push({
+							id: key,
+							title: responseData[key].title,
+							amount: responseData[key].amount
+						})
+					}
+					onLoadIngredients(loadedIngredients);
+				});
+			}
+		},500);
+		return () => {
+			clearTimeout(timer);
+		};
+		/* Si tenes [] como dependencias de useEffect (cuando funciona igual qe componentDidMount),
+			la función para limpiar el timer se ejecuta cuando el componente se desmonta*/
+	}, [enteredFilter, onLoadIngredients, inputRef]); //cambia solo si cambian algunos de estos dos valores pasados como segundo parámetro
+
+	return (
+		<section className="search">
+		<Card>
+			<div className="search-input">
+			<label>Filter by Title</label>
+			<input	type="text"
+					ref={inputRef}
+					value={enteredFilter}
+					onChange={event=>setEnteredFilter(event.target.value)}
+			/>
+			</div>
+		</Card>
+		</section>
+	);
 });
 
 export default Search;
